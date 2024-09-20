cd5793d540f739beec0f0ede87d28a4a2d201ea6
AcaciaSanchez_Xanina <xanadigital14@gmail.com>
2024-09-20 15:18:52 +0200

    TERMINADOS EJ. 1, 2, 9 y 10

--- entradaUsuario.java
+++ entradaUsuario.java
@@ -1,16 +1,47 @@
 /* 5. Leer y mostrar entrada del usuario: Escribe un programa que lea un entero,
 un número decimal y una cadena de texto ingresados por el usuario, luego
 muéstralos.
+
+
 6. Suma de dos números: Escribe un programa que lea dos números enteros del
 usuario e imprima su suma. */
 
 public class entradaUsuario {
     public static void main(String[] args) {
 
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.printLn("Dime un número entero");
+        System.out.printLn("Ahora dime un número con decimales");
 
         
 
     }
 }
+
+        /* Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like " + food);
+        scanner.close(); */
+
+
+        // User input
+        /* Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like " + food);
+        scanner.close(); */
